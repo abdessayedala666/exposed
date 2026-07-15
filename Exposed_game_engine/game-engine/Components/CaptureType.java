0f80e92cd173f4d8eb5f0cb38284f64676878bef
abdessayedala666 <alaabdessaied@gmail.com>
2026-07-15 06:30:05 +0200

    1

--- Exposed_game_engine/game-engine/Components/CaptureType.java
+++
@@ -1,4 +0,0 @@
-public enum CaptureType {
-    CAPTURE_FROM_PILE ,
-    CAPTURE_FROM_OPPONENT
-}
--- Exposed_game_engine/game-engine/Components/Suit.java
+++
@@ -1,8 +0,0 @@
-public enum Suit {
-    CLUBS , 
-    DIAMONDS , 
-    HEARTS , 
-    SPADES   
-
-    
-}
